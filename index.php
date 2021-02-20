72b91b0da591601a937f48b4ffd798139a70f97b
umanari145 <umanari145@gmail.com>
2021-02-20 21:36:39 +0900

    viewのセット

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+//.env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 $app = AppFactory::create();
 require_once __DIR__ . '/routing/base.php';
 $app->run();
--- routing/base.php
+++ routing/base.php
@@ -2,8 +2,13 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
-$app->get("/hoge", function (Request $request, Response $response, array $args) {
-    echo 'aaaaa';
+$name = $_ENV['APP_DIR'];
+$templateDir = sprintf('%s/view', $_ENV['APP_DIR']);
+$twig = Twig::create($templateDir);
+
+$app->get("/hoge", function (Request $request, Response $response, array $args) use ($twig) {
+    $response = $twig->render($response, "index.html");
     return $response;
 });
