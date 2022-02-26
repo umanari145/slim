57d18d6271697192f6ad9f92b8b625f1df7970fb
umanari145 <umanari145@gmail.com>
2022-02-26 21:06:38 +0900

    ログのコンテナを追加

--- config/container.php
+++ config/container.php
@@ -6,6 +6,10 @@ use DI\Container;
 use Slim\Views\Twig;
 use Slim\Factory\AppFactory;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+
 //use Lib\Person\PersonInterface;
 //use Lib\Person\Man;
 //use Lib\Person\Woman;
@@ -54,6 +58,36 @@ $container->set("db",
         $capsule->bootEloquent();
 
         return $capsule;
+    }
+);
+
+// loggerのセット
+$container->set("log",
+    function () {
+        $log = new Logger('default');
+
+        $date_format = 'Y/m/d H:i:s';
+        $format = "%datetime% > %level_name% > %message% %context% %extra%\n";
+
+        $formatter = new LineFormatter($format, $date_format);
+        
+        $logPath = sprintf('%s/logs/%s-%s.log', $_ENV['APP_DIR'], 'debug', date('Y-m-d'));
+        if (!file_exists($logPath)) {
+            touch($logPath);
+        }
+
+        $stream = new StreamHandler($logPath, Logger::INFO);
+        $stream->setFormatter($formatter);
+        //出力先(通常ログ)
+        $log->pushHandler($stream);
+
+        //メモリ情報や付属的に出す情報を出力
+        $log->pushProcessor(function ($record) {
+            $memorySize = (int) (memory_get_usage() / (1024 * 1024)) . "MB";
+            $record['extra']['memory'] = $memorySize;
+            return $record;
+        });
+        return $log;
     }
 );
 
